3a514300ea35cfd4f052e8eb778428e19037a7b2
Adrian Lewis <adrian.lewis@immediate.co.uk>
2019-10-14 14:09:52 +0100

    Minor tweaks to jest/enzyme configuration

--- src/im-access-identitymanager-frontend/enzyme.js
+++ src/im-access-identitymanager-frontend/enzyme.js
@@ -1,3 +1,4 @@
+const enzyme = require("enzyme");
 const Adapter = require('enzyme-adapter-react-16');
 
-require('enzyme').configure({adapter: new Adapter()});
+enzyme.configure({ adapter: new Adapter() });
